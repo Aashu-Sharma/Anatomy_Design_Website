b8317d0769f05bce0c39e1f6592778ed37850176
Aashu-Sharma <sujalsharma7804@gmail.com>
2025-04-04 09:50:02 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,150 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="container">
+      <div className="headline">
+        <h1>Our Anatomy Designs</h1>
+      </div>
+
+      <div className="design-box">
+
+        <div className="card">
+          <div className="imgBox">
+            <img src="https://th.bing.com/th/id/OIP.rLG4pu7gkoIPlHgLa2zJ1wHaEo?rs=1&pid=ImgDetMain" alt="" />
+          </div>
+          <div className="text">
+            <h2>Lorem ipsum dolor sit amet.</h2>
+          </div>
+        </div>
+
+        <div className="card">
+          <div className="imgBox">
+            <img src="https://th.bing.com/th/id/OIP.rLG4pu7gkoIPlHgLa2zJ1wHaEo?rs=1&pid=ImgDetMain" alt="" />
+          </div>
+          <div className="text">
+            <h2>Lorem ipsum dolor sit amet.</h2>
+          </div>
+        </div>
+
+        <div className="card">
+          <div className="imgBox">
+            <img src="https://th.bing.com/th/id/OIP.rLG4pu7gkoIPlHgLa2zJ1wHaEo?rs=1&pid=ImgDetMain" alt="" />
+          </div>
+          <div className="text">
+            <h2>Lorem ipsum dolor sit amet.</h2>
+          </div>
+        </div>
+
+        <div className="card">
+          <div className="imgBox">
+            <img src="https://th.bing.com/th/id/OIP.rLG4pu7gkoIPlHgLa2zJ1wHaEo?rs=1&pid=ImgDetMain" alt="" />
+          </div>
+          <div className="text">
+            <h2>Lorem ipsum dolor sit amet.</h2>
+          </div>
+        </div>
+
+        <div className="card">
+          <div className="imgBox">
+            <img src="https://th.bing.com/th/id/OIP.rLG4pu7gkoIPlHgLa2zJ1wHaEo?rs=1&pid=ImgDetMain" alt="" />
+          </div>
+          <div className="text">
+            <h2>Lorem ipsum dolor sit amet.</h2>
+          </div>
+        </div>
+
+        <div className="card">
+          <div className="imgBox">
+            <img src="https://th.bing.com/th/id/OIP.rLG4pu7gkoIPlHgLa2zJ1wHaEo?rs=1&pid=ImgDetMain" alt="" />
+          </div>
+          <div className="text">
+            <h2>Lorem ipsum dolor sit amet.</h2>
+          </div>
+        </div>
+
+        <div className="card">
+          <div className="imgBox">
+            <img src="https://th.bing.com/th/id/OIP.rLG4pu7gkoIPlHgLa2zJ1wHaEo?rs=1&pid=ImgDetMain" alt="" />
+          </div>
+          <div className="text">
+            <h2>Lorem ipsum dolor sit amet.</h2>
+          </div>
+        </div>
+
+        <div className="card">
+          <div className="imgBox">
+            <img src="https://th.bing.com/th/id/OIP.rLG4pu7gkoIPlHgLa2zJ1wHaEo?rs=1&pid=ImgDetMain" alt="" />
+          </div>
+          <div className="text">
+            <h2>Lorem ipsum dolor sit amet.</h2>
+          </div>
+        </div>
+
+        <div className="card">
+          <div className="imgBox">
+            <img src="https://th.bing.com/th/id/OIP.rLG4pu7gkoIPlHgLa2zJ1wHaEo?rs=1&pid=ImgDetMain" alt="" />
+          </div>
+          <div className="text">
+            <h2>Lorem ipsum dolor sit amet.</h2>
+          </div>
+        </div>
+
+        <div className="card">
+          <div className="imgBox">
+            <img src="https://th.bing.com/th/id/OIP.rLG4pu7gkoIPlHgLa2zJ1wHaEo?rs=1&pid=ImgDetMain" alt="" />
+          </div>
+          <div className="text">
+            <h2>Lorem ipsum dolor sit amet.</h2>
+          </div>
+        </div>
+
+        <div className="card">
+          <div className="imgBox">
+            <img src="https://th.bing.com/th/id/OIP.rLG4pu7gkoIPlHgLa2zJ1wHaEo?rs=1&pid=ImgDetMain" alt="" />
+          </div>
+          <div className="text">
+            <h2>Lorem ipsum dolor sit amet.</h2>
+          </div>
+        </div>
+      </div>
+
+      <div className="footer">
+        <div className="box box2">
+          <h2>CONTACTS</h2>
+          <p className="">
+            EMAIL:{" "}
+            shivangi2020jyotish@gmail.com <br />pranprakashsharma@gmail.com
+          </p>
+          <p className="">
+            PHONE: 7983314136, 9997525012
+          </p>
+        </div>
+        <div className="socialLinks">
+          <div className="links">
+            <div className="icon">
+
+              <a href="https://www.facebook.com/profile.php?id=100068011635055" target="_blank"><i class="fa-brands fa-facebook"></i></a>
+
+            </div>
+            <div className="icon">
+
+              <a href="https://www.youtube.com/@shivangijyotish6572" target="_blank"><i class="fa-brands fa-youtube"></i></a>
+            </div>
+            <div className="icon">
+
+              <a href="https://x.com/JyotishShivangi" target="_blank">
+                <i class="fa-brands fa-square-x-twitter"></i>
+              </a>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
